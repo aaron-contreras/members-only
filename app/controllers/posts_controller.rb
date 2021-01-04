b675995ac36ff86a1f8a3cfea8928e6f0b7a532c
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-04 11:47:40 -0500

    Add recent first scope to Post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_member!, only: [:new, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.most_recent_first
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :member
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { maximum: 140 }
+  scope :most_recent_first, -> { order(created_at: :desc) }
 end

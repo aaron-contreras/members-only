5556a7434859904f4cdd2031bfe720c3211b7dab
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-04 09:52:58 -0500

    Add development gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # Add Rack::LiveReload
+  config.middleware.insert_before Rack::Lock, Rack::LiveReload
 end

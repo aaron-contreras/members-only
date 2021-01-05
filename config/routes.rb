002bfefeed6721780f15e1b4952bbd64603107f8
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-05 17:24:36 -0500

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :members
   resources :posts, only: [:index, :new, :create]
   root 'posts#index'
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
